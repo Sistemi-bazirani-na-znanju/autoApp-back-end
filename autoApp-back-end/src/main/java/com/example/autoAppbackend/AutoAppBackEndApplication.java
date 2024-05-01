6fe440f90035026e6a76b2abe7d91522fdf23f66
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-05-01 19:20:18 +0100

    created project

---
+++ autoApp-back-end/src/main/java/com/example/autoAppbackend/AutoAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAppBackEndApplication.class, args);
+	}
+
+}
---
+++ autoApp-back-end/src/test/java/com/example/autoAppbackend/AutoAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
