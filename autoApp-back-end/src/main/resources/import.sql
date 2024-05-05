1baff431bbb7e61a63f967c4043d0032c6e067da
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-05-05 11:50:52 +0100

    finished vehicle display

--- autoApp-back-end/src/main/resources/import.sql
+++ autoApp-back-end/src/main/resources/import.sql
@@ -6,4 +6,4 @@ INSERT INTO app_user(first_name, last_name, email, password, role_id) VALUES ('A
 --password je "password123"
 
 INSERT INTO vehicle (model_name, current_state, price, imageurl) VALUES ('Model 1', 1, 15000.0, 'https://hips.hearstapps.com/hmg-prod/images/honda-prelude-concept-front-three-quarters-653927960f1f4.jpg?crop=1.00xw:0.920xh;0,0.0801xh&resize=980:*');
-INSERT INTO vehicle (model_name, current_state, price, imageurl) VALUES ('Model 2', 2, 10000.0, 'https://hips.hearstapps.com/hmg-prod/images/honda-prelude-concept-front-three-quarters-653927960f1f4.jpg?crop=1.00xw:0.920xh;0,0.0801xh&resize=980:*');
+INSERT INTO vehicle (model_name, current_state, price, imageurl) VALUES ('Model 2', 2, 10000.0, 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg');
